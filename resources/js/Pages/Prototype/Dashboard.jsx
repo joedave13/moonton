e49c7a9cdc65601a0548696833975c505fe011a1
Joshua Davian <joshuadavian@gmail.com>
2022-09-10 15:40:37 +0700

    browse movie

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -21,6 +21,7 @@ export default function Dashboard() {
                     rel="stylesheet"
                     href="https://unpkg.com/flickity@2/dist/flickity.min.css"
                 />
+                <title>Dashboard</title>
             </Head>
             <div>
                 <div className="font-semibold text-[22px] text-black mb-4">
@@ -39,6 +40,50 @@ export default function Dashboard() {
                         />
                     ))}
                     {/* End Movie Thumbnail */}
+                </Flickity>
+            </div>
+            <div className="mt-[50px]">
+                <div className="font-semibold text-[22px] text-black mb-4">
+                    Browse
+                </div>
+                <Flickity className="gap-[30px]" options={flickityOptions}>
+                    {[1, 2, 3, 4, 5, 6].map((i) => (
+                        <div
+                            className="absolute group overflow-hidden mr-[30px]"
+                            key={i}
+                        >
+                            <img
+                                src="/images/browse-1.png"
+                                className="object-cover rounded-[30px] h-[340px] w-[250px]"
+                                alt=""
+                            />
+                            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px] rounded-br-[28px]">
+                                <div className="px-7 pb-7">
+                                    <div className="font-medium text-xl text-white">
+                                        Meong Golden
+                                    </div>
+                                    <p className="mb-0 text-gray-300 text-base mt-[10px]">
+                                        Horror • Love
+                                    </p>
+                                </div>
+                            </div>
+                            <div
+                                className="absolute top-1/2 left-1/2 -translate-y-[500px] group-hover:-translate-y-1/2
+                                -translate-x-1/2 z-20 transition ease-in-out duration-500"
+                            >
+                                <img
+                                    src="/icons/ic_play.svg"
+                                    className=""
+                                    width="50"
+                                    alt=""
+                                />
+                            </div>
+                            <a
+                                href="watching.html"
+                                className="inset-0 absolute z-50"
+                            ></a>
+                        </div>
+                    ))}
                 </Flickity>
             </div>
         </Authenticated>
