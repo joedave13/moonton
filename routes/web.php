9fdd639c3693f91b54c072c7b2e0358435b93a41
Joshua Davian <joshuadavian@gmail.com>
2022-09-10 20:09:41 +0700

    dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\User\DashboardController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -17,9 +18,12 @@ use Inertia\Inertia;
 
 Route::redirect('/', 'login');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('User/Dashboard/Index');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return Inertia::render('User/Dashboard/Index');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'role:user'])->prefix('dashboard')->name('user.dashboard.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
+});
 
 Route::prefix('prototype')->name('prototype.')->group(function () {
     Route::get('login', function () {
