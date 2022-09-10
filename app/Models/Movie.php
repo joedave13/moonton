c656d5350aeaf201d52abe3d9280a7ce283d9fcb
Joshua Davian <joshuadavian@gmail.com>
2022-09-10 18:18:55 +0700

    movie model & migration

---
+++ app/Models/Movie.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Movie extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'category',
+        'video_url',
+        'thumbnail',
+        'rating',
+        'is_feature',
+    ];
+}
---
+++ database/migrations/2022_09_10_111330_create_movies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('category');
+            $table->text('video_url');
+            $table->text('thumbnail');
+            $table->float('rating')->default(0);
+            $table->boolean('is_feature')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
