350c5a10653183783147e4cc920e629fdba5fcda
Joshua Davian <joshuadavian@gmail.com>
2022-09-10 15:24:33 +0700

    featured movie component

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,3 +1,4 @@
+import FeaturedMovie from "@/Components/FeaturedMovie";
 import Authenticated from "@/Layouts/Authenticated/index";
 import { Head } from "@inertiajs/inertia-react";
 import Flickity from "react-flickity-component";
@@ -28,52 +29,14 @@ export default function Dashboard() {
                 <Flickity className="gap-[30px]" options={flickityOptions}>
                     {/* Movie Thumbnail */}
                     {[1, 2, 3, 4].map((i) => (
-                        <div
-                            className="absolute overflow-hidden group mr-[30px]"
+                        <FeaturedMovie
                             key={i}
-                        >
-                            <img
-                                src="/images/featured-1.png"
-                                className="object-cover rounded-[30px] w-[520px] h-[340px]"
-                                alt=""
-                            />
-                            {/* Rating */}
-                            <div className="rating absolute top-0 left-0">
-                                <div className="p-[30px] flex items-center gap-1">
-                                    <img src="/icons/ic_star.svg" alt="" />
-                                    <span className="text-sm font-medium text-white mt-1">
-                                        4.5/5.0
-                                    </span>
-                                </div>
-                            </div>
-                            {/* End Rating */}
-                            {/* Bottom Detail */}
-                            <div
-                                className="absolute bottom-0 h-[100px] left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px]
-                                rounded-br-[28px] flex justify-between items-center px-7 h-[130px]"
-                            >
-                                <div>
-                                    <div className="font-medium text-[22px] text-white">
-                                        The Batman in Love
-                                    </div>
-                                    <p className="mb-0 text-white text-sm font-light">
-                                        Action • Horror
-                                    </p>
-                                </div>
-                                <div className="translate-x-[100px] group-hover:translate-x-0 transition ease-in-out duration-500">
-                                    <img
-                                        src="/icons/ic_play.svg"
-                                        width="50"
-                                        alt=""
-                                    />
-                                </div>
-                            </div>
-                            {/* End Bottom Detail */}
-                            <a
-                                href="watching.html"
-                                className="inset-0 absolute z-50"
-                            ></a>
-                        </div>
+                            slug="the-batman-in-love"
+                            title={`The Batman In Love ${i}`}
+                            category="Comedy"
+                            thumbnail="https://picsum.photos/id/1/300/300"
+                            rating={i + 1}
+                        />
                     ))}
                     {/* End Movie Thumbnail */}
                 </Flickity>
