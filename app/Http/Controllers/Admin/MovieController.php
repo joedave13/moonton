a579020c22d0f17a1a972da32cde3d1721d4b1c7
Joshua Davian <joshuadavian@gmail.com>
2022-09-11 16:45:43 +0700

    admin init create movie

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -26,7 +26,7 @@ class MovieController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Admin/Movie/Create');
     }
 
     /**
--- resources/js/Pages/Admin/Movie/Index.jsx
+++ resources/js/Pages/Admin/Movie/Index.jsx
@@ -1,3 +1,16 @@
-export default function Index() {
-    return <h1>Admin Page</h1>;
+import Button from "@/Components/Button";
+import "../../../../css/button.css";
+import Authenticated from "@/Layouts/Authenticated/index";
+import { Link } from "@inertiajs/inertia-react";
+
+export default function Index({ auth }) {
+    return (
+        <Authenticated auth={auth}>
+            <Link href={route("admin.dashboard.movie.create")}>
+                <Button type="button" className="w-44 mb-8">
+                    Create New Movie
+                </Button>
+            </Link>
+        </Authenticated>
+    );
 }
