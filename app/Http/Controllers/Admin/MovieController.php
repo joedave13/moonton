0a6f4e696b3e00dfaba80185c8159a0368fa1f1c
Joshua Davian <joshuadavian@gmail.com>
2022-09-11 17:50:25 +0700

    admin movie store validation

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Movie\StoreRequest;
 use App\Models\Movie;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -35,7 +36,7 @@ class MovieController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreRequest $request)
     {
         return $request->all();
     }
