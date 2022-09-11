58e1c49e0f3a1bc0c1ede108cded6cfea54c500d
Joshua Davian <joshuadavian@gmail.com>
2022-09-11 18:25:47 +0700

    admin flash message

--- resources/js/Pages/Admin/Movie/Index.jsx
+++ resources/js/Pages/Admin/Movie/Index.jsx
@@ -1,16 +1,23 @@
 import Button from "@/Components/Button";
 import "../../../../css/button.css";
 import Authenticated from "@/Layouts/Authenticated/index";
-import { Link } from "@inertiajs/inertia-react";
+import { Head, Link } from "@inertiajs/inertia-react";
+import FlashMessage from "@/Components/FlashMessage";
 
-export default function Index({ auth }) {
+export default function Index({ auth, flashMessage }) {
     return (
         <Authenticated auth={auth}>
+            <Head>
+                <title>Movie List</title>
+            </Head>
             <Link href={route("admin.dashboard.movie.create")}>
                 <Button type="button" className="w-44 mb-8">
                     Create New Movie
                 </Button>
             </Link>
+            {flashMessage?.message && (
+                <FlashMessage message={flashMessage.message} />
+            )}
         </Authenticated>
     );
 }
